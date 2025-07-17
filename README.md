819ec4c2c7d70bbfc1f28406de661f88bb013f8a
tobilusamna <tobilusamna@gmail.com>
2025-07-17 14:02:30 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bokepindosd
-bokepindosd
+# [bokepindosd](https://bokepindosd.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bokep Indo Sd';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
+export const url = 'https://bokepindosd.pages.dev';
+export const terbit = '2025-01-11T00:01:03Z';
+
+export const google = 'oh1TWfZ-OJeiFzIe146Jch9Czpg4y0TYJP--t5l5BdI';
+export const yandex = 'dbe73662e3b09625'
+
+export const index = '16eac6db-499e-4776-a9e8-1534513be4b7';
